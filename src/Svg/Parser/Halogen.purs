20d9588857682a735578355f899749064a08b793
rnons <remotenonsense@gmail.com>
2017-12-17 16:32:51 +0900

    use IProp r i, add icon helper method in Svg.Parser.Halogen

--- src/Svg/Parser/Halogen.purs
+++ src/Svg/Parser/Halogen.purs
@@ -1,24 +1,32 @@
-module Svg.Parser.Halogen where
+module Svg.Parser.Halogen
+  ( svgElementToHtml
+  , svgElementToHtmlWithAttrs
+  , svgNodeToHtml
+  , parse
+  , icon
+  ) where
 
 import Prelude
 import Data.Bifunctor (lmap)
 import Data.Array (fromFoldable)
-import Data.Either (Either)
+import Data.Either (Either(Right))
 import Data.Maybe (Maybe(..))
-import Halogen.HTML (HTML, text)
-import Halogen.HTML.Core (Namespace(Namespace), ElemName(ElemName), Prop,
-                          AttrName(AttrName), element, attr)
+import Halogen.HTML (HTML, IProp, Namespace(Namespace), ElemName(ElemName),
+                     AttrName(AttrName), text, elementNS, attr)
 import Svg.Parser (SvgNode(..), Element, SvgAttribute(..), parseToSvgNode)
 
-ns :: Maybe Namespace
-ns = Just $ Namespace "http://www.w3.org/2000/svg"
+ns :: Namespace
+ns = Namespace "http://www.w3.org/2000/svg"
 
-svgAttributeToProp :: forall i. SvgAttribute -> Prop i
-svgAttributeToProp (SvgAttribute k v) = attr Nothing (AttrName k) v
+svgAttributeToProp :: forall r i. SvgAttribute -> IProp r i
+svgAttributeToProp (SvgAttribute k v) = attr (AttrName k) v
 
 svgElementToHtml :: forall p i. Element -> HTML p i
-svgElementToHtml ele =
-  element ns (ElemName ele.name) attrs children
+svgElementToHtml ele = svgElementToHtmlWithAttrs ele []
+
+svgElementToHtmlWithAttrs :: forall p r i. Element -> Array (IProp r i) -> HTML p i
+svgElementToHtmlWithAttrs ele newAttrs =
+  elementNS ns (ElemName ele.name) (attrs <> newAttrs) children
   where
     attrs = fromFoldable $ svgAttributeToProp <$> ele.attributes
     children = fromFoldable $ svgNodeToHtml <$> ele.children
@@ -31,3 +39,10 @@ svgNodeToHtml (SvgComment str) = text ""
 parse :: forall p i. String -> Either String (HTML p i)
 parse input =
   lmap show $ svgNodeToHtml <$> parseToSvgNode input
+
+icon :: forall p r i. String -> Array (IProp r i) -> HTML p i
+icon input attrs =
+  case parseToSvgNode input of
+    Right (SvgElement element) -> svgElementToHtmlWithAttrs element attrs
+    _ -> text ""
+
